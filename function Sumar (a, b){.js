0a3ab6a790abc0adf7cabc97ff2579b57948e9d1
Jean Evins <130567240+jeanvergez@users.noreply.github.com>
2025-02-10 12:32:21 -0400

    Add files via upload

---
+++ function Sumar (a, b){.js
@@ -0,0 +1,16 @@
+function sumar (a , b){
+
+        
+    return a + b
+    
+    
+}
+
+let resultado = sumar(20,12)
+console.log("La suma de a y b es", resultado)
+ 
+ 
+
+
+const suma =( a , b) => a + b;
+console.log( "La suma es", suma(20,10));
